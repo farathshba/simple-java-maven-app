8f01b3e3a2dc92f9b2254eb61ff6ce4586faca1e
Farath <farisoft@gmail.com>
2024-01-30 11:22:31 +0800

    changes with v10.3

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -1,5 +1,7 @@
 package com.mycompany.app;
 
+import java.io.File;
+import java.io.IOException;
 /**
  * Hello world!
  */
@@ -11,6 +13,18 @@ public class App {
 
     public static void main(String[] args) {
         System.out.println(MESSAGE);
+
+        try {
+
+            File file = new File(".env");
+            if (file.createNewFile()) {
+                System.out.println("File is created!");
+            } else {
+                System.out.println("File already exists.");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     public String getMessage() {
